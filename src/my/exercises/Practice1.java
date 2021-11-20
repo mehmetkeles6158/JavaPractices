b9d359b4e5bb40664224eb2357c3a1801b87d52e
Mehmet <mehmetk6158@gmail.com>
2021-11-20 11:01:41 -0500

    java practice -1 reverse string and array

--- src/my/exercises/Practice1.java
+++ src/my/exercises/Practice1.java
@@ -1,12 +1,32 @@
 package my.exercises;
 
+import java.util.Arrays;
+
 public class Practice1 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
+//        System.out.println("Hello World!");
+//
         int [] numbers = {1,2,3,4,5};
+        int [] reverse = new int[numbers.length];
+        int n = 0 ;
         for(int i = numbers.length - 1; i < numbers.length && i >= 0; i--){
-            System.out.println(numbers[i]);
+            reverse[n] = numbers[i];
+            n++;
         }
+
+        System.out.println(Arrays.toString(reverse));
+
+        String name = "mehmet";
+        char first_letter = name.charAt(0);
+        System.out.println(first_letter);
+
+        String  backwards = "";
+
+        for( int i = name.length()-1; i >= 0; i--){
+
+         backwards = backwards + name.charAt(i);
+
+        }
+        System.out.println(backwards);
     }
 }
