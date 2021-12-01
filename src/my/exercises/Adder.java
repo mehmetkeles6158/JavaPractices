4a81bf2338a481989eac7bb612dd6a2112512bdb
Mehmet <mehmetk6158@gmail.com>
2021-12-01 12:25:30 -0500

    OOP, Method Overloading

---
+++ src/my/exercises/Adder.java
@@ -0,0 +1,26 @@
+package my.exercises;
+
+public class Adder {
+
+//  Method Overloading  - Changing the number of arguments
+
+//    static int add(int a, int b) { return a + b;};
+//    static int add(int a, int b, int c) { return a + b + c;};
+
+    // Method Overloading - Changing data type of arguments
+    static int add(int a, int b){return a+b;}  // 2 arguments of int data type
+    static double add(double a, double b){return a+b;}  // 2 arguments of double data type
+
+}
+
+class TestOverloading1{
+    public static void main(String[] args) {
+//        System.out.println(Adder.add(11,11));
+//        System.out.println(Adder.add(11,11,11));
+
+        // Method Overloading - Changing data type of arguments
+        System.out.println(Adder.add(11,11));
+        System.out.println(Adder.add(12.3,12.6));
+    }
+
+}
