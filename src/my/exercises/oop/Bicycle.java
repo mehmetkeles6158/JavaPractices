5329c44e60243034b9b7d5953b909f7ab48e88d6
Mehmet <mehmetk6158@gmail.com>
2022-01-18 22:38:52 -0500

    Add bicycle class(fields and methods)

---
+++ src/my/exercises/oop/Bicycle.java
@@ -0,0 +1,10 @@
+package my.exercises.oop;
+
+public class Bicycle {
+//    state or field
+    private int gear = 5 ;
+//    behavior or method
+    public void braking() {
+        System.out.println("Working of Braking");
+    }
+}
